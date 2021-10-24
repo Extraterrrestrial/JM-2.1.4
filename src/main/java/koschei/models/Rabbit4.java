b2fc5b9a17a4ce1d0260cb568a012ccf59ad8951
Yuri Kriger <yuri.kriger@gmail.com>
2021-10-24 23:09:17 +0600

    Rabbit4

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,9 +1,17 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
+
+    private Duck5 duck;
+    @Autowired
+    public void setDuck(Duck5 duck){
+        this.duck = duck;
+    }
+
 
     @Override
     public String toString() {
