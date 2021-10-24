210b4bd62edac631f333ed851856cc65321317b7
Yuri Kriger <yuri.kriger@gmail.com>
2021-10-24 23:02:44 +0600

    Duck5

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,9 +1,13 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
+    @Autowired
+    private Egg6 egg;
+
 
     @Override
     public String toString() {
